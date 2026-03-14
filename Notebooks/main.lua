a513e4cc716048e077f9b729726e6b0294f07e53
Gitionia <67328894+Gitionia@users.noreply.github.com>
2026-03-14 23:53:03 +0100

    Updated main.lua file to support new plugin api as well as be more robust on linux. Not tested on windows yet

--- Notebooks/main.lua
+++ Notebooks/main.lua
@@ -1,251 +1,314 @@
---- Change to your needs:
-notebookRootDir = ""
-sep = "\\"     --- seperator in filepaths: "\\" should work for windows, for linux try "/"
+--- Configure plugin:
+notebookRootDir = ""  -- the root directory for the notebooks
+platform = "linux" -- set platform: "linux" or "windows"
+version_above_1_3 = false -- default is false, set to true if warning appears when using plugin
 --------------------------------------------
 
 --- Shortcuts and icons can be changed here
 function initUi()
-  app.registerUi({["menu"] = "Previous Page", ["callback"] = "switchToPreviousPage", ["accelerator"]="<Alt>Left", ["toolbarId"] = "PreviousPage",  ["iconName"] = "pan-start-symbolic"});
-  app.registerUi({["menu"] = "Next Page", ["callback"] = "switchToNextPage", ["accelerator"]="<Alt>Right", ["toolbarId"] = "NextPage",  ["iconName"] = "pan-start-symbolic-rtl"});
-  app.registerUi({["menu"] = "Previous Notebook", ["callback"] = "switchToPreviousNotebook", ["accelerator"]="<Alt><Shift>Left", ["toolbarId"] = "PreviousNotebook",  ["iconName"] = "pan-up-symbolic"});
-  app.registerUi({["menu"] = "Next Notebook", ["callback"] = "switchToNextNotebook", ["accelerator"]="<Alt><Shift>Right", ["toolbarId"] = "NextNotebook",  ["iconName"] = "pan-down-symbolic"});
-  app.registerUi({["menu"] = "Select File", ["callback"] = "selectFile", ["accelerator"]="<Alt><Shift>k", ["toolbarId"] = "SelectFile",  ["iconName"] = "user-bookmarks"});
-
+	app.registerUi({
+		["menu"] = "Previous Page",
+		["callback"] = "switchToPreviousPage",
+		["accelerator"] = "<Alt>Left",
+		["toolbarId"] = "PreviousPage",
+		["iconName"] = "pan-start-symbolic",
+	})
+	app.registerUi({
+		["menu"] = "Next Page",
+		["callback"] = "switchToNextPage",
+		["accelerator"] = "<Alt>Right",
+		["toolbarId"] = "NextPage",
+		["iconName"] = "pan-start-symbolic-rtl",
+	})
+	app.registerUi({
+		["menu"] = "Previous Notebook",
+		["callback"] = "switchToPreviousNotebook",
+		["accelerator"] = "<Alt><Shift>Left",
+		["toolbarId"] = "PreviousNotebook",
+		["iconName"] = "pan-up-symbolic",
+	})
+	app.registerUi({
+		["menu"] = "Next Notebook",
+		["callback"] = "switchToNextNotebook",
+		["accelerator"] = "<Alt><Shift>Right",
+		["toolbarId"] = "NextNotebook",
+		["iconName"] = "pan-down-symbolic",
+	})
+	app.registerUi({
+		["menu"] = "Select File",
+		["callback"] = "selectFile",
+		["accelerator"] = "<Alt><Shift>k",
+		["toolbarId"] = "SelectFile",
+		["iconName"] = "user-bookmarks",
+	})
 end
 
+--- Do not change after this!
+if platform == "linux" then
+	sep = "/"
+else
+	sep = "\\"
+end
 
+local openDialog
+if version_above_1_3 then
+	function openDialog(message, options, callback, isError)
+		app.openDialog(message, options, callback, isError)
+	end
+else
+	function openDialog(message, options, callback, isError)
+		local selection = app.msgbox(message, options)
+		_G[callback](selection)
+	end
+end
+
+function emptyCallback() end
+function messageDialog(message, options)
+	openDialog(message, options, "emptyCallback", false)
+end
+
+function showMessage(message)
+	openDialog(message, { "Ok" }, "emptyCallback", false)
+end
 
 function selectFile()
-  local notebooks = getNotebooksAndFindIndex()
+	notebooks = getNotebooksAndFindIndex()
 
-  local selection = app.msgbox("Select Notebook", notebooks)
-
-  local notebookDir = getRootDir() .. sep .. notebooks[selection]
-  local files = getFilesInNotebookAndFindIndex("", notebookDir)
-
-  local shortenedFileNames = getShortenedFileNames(files)
-
-  local selectedFile = app.msgbox("Select File", shortenedFileNames)
-
-  local newFilePath = notebookDir .. sep .. files[selectedFile]
-
-  app.openFile(newFilePath)
+	if #notebooks == 0 then
+		showMessage("There are no notebooks")
+	else
+		openDialog("Select notebook", notebooks, "openNotebookAndChooseFile", false)
+	end
 end
 
+function openNotebookAndChooseFile(selection)
+	notebookDir = getRootDir() .. sep .. notebooks[selection]
+	files = getFilesInNotebookAndFindIndex("", notebookDir)
 
----unused
-function selectNotebook()
-  local notebooks = getNotebooksAndFindIndex()
+	if #files == 0 then
+		showMessage("Notebook is empty")
+	else
+		shortenedFileNames = getShortenedFileNames(files)
+		openDialog("Select File", shortenedFileNames, "openSelectedFile", false)
+	end
+end
 
-  local selection = app.msgbox("Select Notebook", notebooks)
+function openSelectedFile(selection)
+	local newFilePath = notebookDir .. sep .. files[selection]
+	app.openFile(newFilePath)
+end
 
-  local notebookDir = getRootDir() .. sep .. notebooks[selection]
-  local files = getFilesInNotebookAndFindIndex("", notebookDir)
-  local newFilePath = notebookDir .. sep .. files[1]
+function switchNotebook(locationStr, chooser)
+	local currentNotebook = getCurrentNotebook()
+	local notebooks, i = getNotebooksAndFindIndex(currentNotebook)
 
-  app.openFile(newFilePath)
+	if i == -2 then
+		showMessage(
+			"Could not find current notebook. Not switching files. \n(Probably the currently open file is not in the notebook root path)"
+		)
+	elseif #notebooks == 0 then
+		showMessage("There are no notebooks")
+	else
+		local newNotebook = notebooks[chooser(notebooks, i)]
+
+		local newNotebookPath = getRootDir() .. sep .. newNotebook
+		local files = getFilesInNotebookAndFindIndex("", newNotebookPath)
+
+		if #files == 0 then
+			showMessage(locationStr .. " notebook '" .. newNotebook .. "' is empty. Not switching files")
+		else
+			local newFilePath = newNotebookPath .. sep .. files[1]
+			app.openFile(newFilePath)
+		end
+	end
 end
 
 function switchToPreviousNotebook()
-  local currentNotebook = getCurrentNotebook()
-  local notebooks, i = getNotebooksAndFindIndex(currentNotebook)
-  
-  local newNotebook
-  if i > 1 then
-    newNotebook = notebooks[i - 1]
-  else
-    newNotebook = notebooks[#notebooks]
-  end
-
-  local newNotebookPath = getRootDir() .. sep .. newNotebook  
-  local files, j = getFilesInNotebookAndFindIndex("", newNotebookPath)
-
-  local newFilePath = newNotebookPath .. sep .. files[1]
-
-  app.openFile(newFilePath)
+	switchNotebook("Previous", function(notebooks, i)
+		if i > 1 then
+			return i - 1
+		else
+			return #notebooks
+		end
+	end)
 end
 
 function switchToNextNotebook()
-  local currentNotebook = getCurrentNotebook()
-  local notebooks, i = getNotebooksAndFindIndex(currentNotebook)
-  
-  local newNotebook
-  if i < #notebooks then
-    newNotebook = notebooks[i + 1]
-  else
-    newNotebook = notebooks[1]
-  end
-
-  local newNotebookPath = getRootDir() .. sep .. newNotebook  
-  local files, j = getFilesInNotebookAndFindIndex("", newNotebookPath)
-
-  local newFilePath = newNotebookPath .. sep .. files[1]
-
-  app.openFile(newFilePath)
+	switchNotebook("Next", function(notebooks, i)
+		if i < #notebooks then
+			return i + 1
+		else
+			return 1
+		end
+	end)
 end
 
+function switchPage(chooser)
+	local currentFile = getCurrentFileName()
+	local files, i = getFilesInNotebookAndFindIndex(currentFile, getCurrentFileFolder())
 
+	if i == -2 then
+		showMessage(
+			"Could not find current file. Not switching files. \n(Probably the currently open file is not in the notebook root path)"
+		)
+	else
+		local newFile = files[chooser(files, i)]
+		local newFilePath = getCurrentFileFolder() .. sep .. newFile
+
+		app.openFile(newFilePath)
+	end
+end
 
 function switchToPreviousPage()
-  local currentFile = getCurrentFileName()
-  local files, i = getFilesInNotebookAndFindIndex(currentFile, getCurrentFileFolder())
-  
-  local newFile
-  if i > 1 then
-    newFile = files[i - 1]
-  else
-    newFile = files[#files]
-  end
-
-  local newFilePath = getCurrentFileFolder() .. sep .. newFile
-
-  app.openFile(newFilePath)
+	switchPage(function(files, i)
+		if i > 1 then
+			return i - 1
+		else
+			return #files
+		end
+	end)
 end
-
 
 function switchToNextPage()
-  local currentFile = getCurrentFileName()
-  local files, i = getFilesInNotebookAndFindIndex(currentFile, getCurrentFileFolder())
-  
-  local newFile
-  if i < #files then
-    newFile = files[i + 1]
-  else
-    newFile = files[1]
-  end
-
-  
-  local newFilePath = getCurrentFileFolder() .. sep .. newFile
-
-  app.openFile(newFilePath)
+	switchPage(function(files, i)
+		if i < #files then
+			return i + 1
+		else
+			return 1
+		end
+	end)
 end
 
-
 function getRootDir()
-  return notebookRootDir
+	return notebookRootDir
 end
 
 function getNotebooksAndFindIndex(notebookToFind)
-  local p = io.popen('dir /b "' .. getRootDir() .. '"')
+	local cmd
+	if platform == "linux" then
+		cmd = 'ls "' .. getRootDir() .. '"'
+	else
+		cmd = 'dir /b "' .. getRootDir() .. '"'
+	end
+	local p = io.popen(cmd)
 
-  local notebookList = {}
-  local index = -2
- 
-  local i = 1
-  for notebook in p:lines() do
-    notebookList[#notebookList+1] = notebook
-    if notebook == notebookToFind then
-      index = i
-    end
-    i = i + 1
-  end
+	local notebookList = {}
+	local index = -2
 
-  return notebookList, index
+	local i = 1
+	for notebook in p:lines() do
+		notebookList[#notebookList + 1] = notebook
+		if notebook == notebookToFind then
+			index = i
+		end
+		i = i + 1
+	end
+
+	return notebookList, index
 end
 
 function getFilesInNotebookAndFindIndex(fileToFind, directory)
-  
-  local p = io.popen('dir /b "' .. directory .. '"')
+	local cmd
+	if platform == "linux" then
+		cmd = 'ls -p "' .. directory .. '"'
+	else
+		cmd = 'dir /b "' .. directory .. '"'
+	end
+	local p = io.popen(cmd)
 
-  local fileList = {}
-  local index = -2
- 
-  local i = 1
-  for file in p:lines() do
-    fileList[#fileList+1] = file
-    if file == fileToFind then
-      index = i
-    end
-    i = i + 1
-  end
+	local fileList = {}
+	local index = -2
 
-  return fileList, index
+	local i = 1
+	for file in p:lines() do
+		fileList[#fileList + 1] = file
+		if file == fileToFind then
+			index = i
+		end
+		i = i + 1
+	end
+
+	return fileList, index
 end
 
-
 function getCurrentFileFolder()
+	local currentFilePath = getCurrentFilePath()
+	local index = string.find(currentFilePath, sep .. "[^" .. sep .. "]*$")
+	if index == nil then
+		index = #currentFilePath
+	end
 
-  local currentFilePath = getCurrentFilePath()
-  local index = string.find(currentFilePath, sep .. "[^" .. sep .. "]*$")
-  if index == nil then
-    index = #currentFilePath
-  end
-  
-  local currentFileFolder = string.sub(currentFilePath, 0, index - 1)
-  return currentFileFolder
-
+	local currentFileFolder = string.sub(currentFilePath, 0, index - 1)
+	return currentFileFolder
 end
 
 function getCurrentNotebook()
-  local folderPath = getCurrentFileFolder()
-  local index = string.find(folderPath, sep .. "[^" .. sep .. "]*$")
+	local folderPath = getCurrentFileFolder()
+	local index = string.find(folderPath, sep .. "[^" .. sep .. "]*$")
 
-  local notebook = string.sub(folderPath, index + 1)
-  return notebook
+	if index == nil then
+		index = #folderPath
+	end
+
+	local notebook = string.sub(folderPath, index + 1)
+	return notebook
 end
 
 function getCurrentFileName()
+	local currentFilePath = getCurrentFilePath()
+	local index = string.find(currentFilePath, sep .. "[^" .. sep .. "]*$")
+	if index == nil then
+		index = #currentFilePath
+	end
 
-  local currentFilePath = getCurrentFilePath()
-  local index = string.find(currentFilePath, sep .. "[^" .. sep .. "]*$")
-  if index == nil then
-    index = #currentFilePath
-  end
-
-  local currentFileFolder = string.sub(currentFilePath, index + 1)
-  return currentFileFolder
-
+	local currentFileFolder = string.sub(currentFilePath, index + 1)
+	return currentFileFolder
 end
 
-
 function getCurrentFilePath()
-
-  local doc = app.getDocumentStructure()
-  return doc.xoppFilename
-
+	local doc = app.getDocumentStructure()
+	return doc.xoppFilename
 end
 
 function getShortenedFileNames(files)
-  local shortenedFileNames = {}
-  
-  local maxLength = string.len(files[1])
-  for i = 1, #files do
-    if string.len(files[i]) > maxLength then
-      maxLength = string.len(files[i])
-    end
-  end
+	local shortenedFileNames = {}
 
-  local estimatedTotalWidth = #files * maxLength
-  local maxTotalWidth = 120
-  local targetWidth = math.floor(maxTotalWidth / #files)
+	local maxLength = string.len(files[1])
+	for i = 1, #files do
+		if string.len(files[i]) > maxLength then
+			maxLength = string.len(files[i])
+		end
+	end
 
-  for i = 1, #files do
-    shortenedFileNames[i] = shortenFileName(files[i], targetWidth)
-  end
+	local estimatedTotalWidth = #files * maxLength
+	local maxTotalWidth = 120
+	local targetWidth = math.floor(maxTotalWidth / #files)
 
-  return shortenedFileNames
+	for i = 1, #files do
+		shortenedFileNames[i] = shortenFileName(files[i], targetWidth)
+	end
+
+	return shortenedFileNames
 end
 
-function shortenFileName(filename, targetWidth) 
-  local index = string.find(filename, ".[^.]*$")
-  local newFileName = string.sub(filename, 1, index - 1)
+function shortenFileName(filename, targetWidth)
+	local index = string.find(filename, ".[^.]*$")
+	local newFileName = string.sub(filename, 1, index - 1)
 
-  if string.len(newFileName) > 3 * targetWidth then
-    newFileName = string.sub(newFileName, 1, 3 * targetWidth - 1) .. "..."
-    newFileName = stringInsert(newFileName, targetWidth, "\n")   
-    newFileName = stringInsert(newFileName, 2 * targetWidth + 1, "\n")   
+	if string.len(newFileName) > 3 * targetWidth then
+		newFileName = string.sub(newFileName, 1, 3 * targetWidth - 1) .. "..."
+		newFileName = stringInsert(newFileName, targetWidth, "\n")
+		newFileName = stringInsert(newFileName, 2 * targetWidth + 1, "\n")
+	elseif string.len(newFileName) > 2 * targetWidth + 2 then
+		newFileName = stringInsert(newFileName, targetWidth, "\n")
+		newFileName = stringInsert(newFileName, 2 * targetWidth, "\n")
+	elseif string.len(newFileName) > targetWidth + 2 then
+		newFileName = stringInsert(newFileName, targetWidth, "\n")
+	end
 
-  elseif string.len(newFileName) > 2 * targetWidth + 2 then
-    newFileName = stringInsert(newFileName, targetWidth, "\n")   
-    newFileName = stringInsert(newFileName, 2 * targetWidth, "\n")   
-
-  elseif string.len(newFileName) > targetWidth + 2 then
-    newFileName = stringInsert(newFileName, targetWidth, "\n")   
-  end
-
-  return newFileName
+	return newFileName
 end
 
 function stringInsert(baseStr, i, insertStr)
-  return string.sub(baseStr, 1, i) .. insertStr .. string.sub(baseStr, i + 1)
+	return string.sub(baseStr, 1, i) .. insertStr .. string.sub(baseStr, i + 1)
 end
